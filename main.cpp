2eb6e2f008bd92858d372150eb906f8d907a5b0c
bdecoute <bart.decoutere@ni.com>
2024-06-13 15:43:07 +0200

    interaction with filter in core

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
 #include <boost/log/trivial.hpp>
 #include <string>
+
+namespace logging = boost::log;
+
+logging::trivial::severity_level severity_level = logging::trivial::info;
+
+void init() {
+    logging::core::get()->set_filter(
+        logging::trivial::severity >= severity_level
+    );
+}
 
 std::string get_string() {
     BOOST_LOG_TRIVIAL(debug) << "Hello, world will be printed soon!";
@@ -7,6 +19,8 @@ std::string get_string() {
 }
 
 int main() {
+    init();
+
     BOOST_LOG_TRIVIAL(info) << get_string();
     return 0;
 }
